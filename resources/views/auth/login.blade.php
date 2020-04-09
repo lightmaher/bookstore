cb315917634ecd70c2a9d5659e60e4542238b8b1
lightmaher <lightmaher3@gmail.com>
2020-04-10 01:59:36 +0200

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts.app')
+
+@section('content')
+<head>
+<style>
+img.background {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  z-index: -1;
+  filter: blur(10px);
+}
+
+body {
+    font-family: "Lato", sans-serif;
+  
+  
+    
+
+    background-size: 100% ;
+    
+}
+
+.main-head{
+    height: 150px;
+    background: #FFF;
+   
+}
+
+.sidenav {
+    height: 100%;
+    background-color: black;
+
+
+
+;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+
+.main {
+    padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+}
+
+@media screen and (max-width: 450px) {
+    .login-form{
+        margin-top: 10%;
+    }
+
+    .register-form{
+        margin-top: 10%;
+    }
+}
+
+@media screen and (min-width: 768px){
+    .main{
+        margin-left: 40%; 
+    }
+
+    .sidenav{
+        margin-top:62px;
+        width: 20%;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+    }
+
+    .login-form{
+        margin-top: 80%;
+    }
+
+    .register-form{
+        margin-top: 20%;
+    }
+}
+
+
+.login-main-text{
+    margin-top: 20%;
+    padding: 60px;
+    color: #fff;
+}
+
+.login-main-text h2{
+    font-weight: 300;
+}
+
+.btn-black{
+    background-color: #000 !important;
+    color: #fff;
+}
+
+
+</style>
+</head>
+<body >
+<img class="background" src="book14.jpg" >
+
+<div class="sidenav" >
+
+<div class="login-main-text" >
+   <h2 >Book Store<br> Login Page</h2>
+
+</div>
+</div>
+      <div class="main"  >
+         <div class="col-md-6 col-sm-12">
+       
+            <div class="login-form">
+             
+             <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                  <div class="form-group">
+                     <label>E-Mail</label>
+                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                @error('email')
+                 <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+                  </div>
+                  <div class="form-group">
+                     <label>Password</label>
+                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+              @error('password')
+              <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+                  </div>
+                  <button type="submit" class="btn btn-black">Login</button>
+               
+                  <a class="btn btn-secondary" href="{{ route('register') }}">{{ __('Register') }}</a>
+               </form>
+            </div>
+         </div>
+      </div>
+      </body>
+      @endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,171 @@
+@extends('layouts.app')
+
+@section('content')
+<head>
+<style>
+label{
+    color:black;
+}
+img.background {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  z-index: -1;
+  filter: blur(10px);
+}
+body {
+    font-family: "Lato", sans-serif;
+}
+
+
+
+.main-head{
+    height: 150px;
+    background: #FFF;
+   
+}
+
+.sidenav {
+    height: 100%;
+    background-color: black;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+
+.main {
+    padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+}
+
+@media screen and (max-width: 450px) {
+    .login-form{
+        margin-top: 10%;
+    }
+
+    .register-form{
+        margin-top: 10%;
+    }
+}
+
+@media screen and (min-width: 768px){
+    .main{
+        margin-left: 40%; 
+    }
+
+    .sidenav{
+         margin-top:62px;
+        width: 20%;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+    }
+
+    .login-form{
+        margin-top: 80%;
+    }
+
+    .register-form{
+        margin-top: 20%;
+    }
+}
+
+
+.login-main-text{
+    margin-top: 20%;
+    padding: 60px;
+    color: #FFFAF0
+;
+}
+
+.login-main-text h2{
+    font-weight: 300;
+}
+
+.btn-black{
+    background-color: #000 !important;
+    color: #fff;
+}
+</style>
+</head>
+<body >
+
+ <img class="background" src="book14.jpg" >
+
+      <div class="main">
+         <div class="col-md-6 col-sm-12">
+       
+            <div class="login-form">
+              <div class="sidenav">
+         <div class="login-main-text">
+            <h2 >Book Store<br> Register Page</h2>
+    
+         </div>
+      </div>
+      <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class=" col-md-6 col-form-label ">{{ __('Name') }}</label>
+
+                            <div class="col-md-12">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-6 col-form-label ">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label ">{{ __('Password') }}</label>
+
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-6 col-form-label ">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-12">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                  
+                  <button type="submit" class="btn btn-black">Register</button>
+               </form>
+            </div>
+         </div>
+      </div>
+      </body>
+      @endsection
